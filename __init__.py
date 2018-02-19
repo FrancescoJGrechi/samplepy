9a5f67fb09848ab285b67605bca7f1f43d9ed83d
J. Michael Burgess <jmichaelburgess@gmail.com>
2018-02-19 08:13:28 +0100

    changing structure

--- __init__.py
+++
@@ -1,8 +0,0 @@
-
-#from __future__ import absolute_import
-
-from rejection import Rejection
-from importance import Importance
-from metropolis_hastings import MH
-
-__all__= ['MH', 'Importance', 'Rejection']
